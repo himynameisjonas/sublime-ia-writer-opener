3f39b081e5309be9e68d426847f14b48e1974387
Jonas Forsberg <himynameisjonas@gmail.com>
2012-07-21 20:10:18 +0200

    Fix command error

--- ia-writer-opener.py
+++ ia-writer-opener.py
@@ -5,17 +5,15 @@ class OpenInIaWriterCommand(sublime_plugin.WindowCommand):
   def get_path(self):
     if self.window.active_view():
       return self.window.active_view().file_name()
-    elif self.window.folders():
-      return self.window.folders()[0]
     else:
-      sublime.status_message(__name__ + ': No place to open GitX to')
+      sublime.status_message(__name__ + ': No file')
       return False
 
   def is_enabled(self):
     return True
 
   def run(self, *args):
-    print "hej"
+    print "Opening in iA Writer"
     path = self.get_path()
     if not path:
       return
